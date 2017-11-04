f4b638ba2f4ca25b4aa7c63d885b4d38d3db103d
Tim Pinkerton <timpinkerton@mail.com>
2017-11-04 14:14:07 -0400

    added delete function

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^details/(?P<id>\w{0,50})/$', views.details),
     url(r'^add', views.add, name='add'),
-    url(r'^edit/(?P<id>\w{0,50})/$', views.edit, name='edit')
+    url(r'^edit/(?P<id>\w{0,50})/$', views.edit, name='edit'),
+    url(r'^delete/(?P<id>\w{0,50})/$', views.delete, name='delete')
 ]
